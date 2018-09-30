07f65298fb401fd8356ed2a4202b6a068b156d0a
Mohamed Hussein <mohamed_hussein5@live.com>
2018-09-30 04:07:32 +0200

    after adding the main entities.

---
+++ MAM/src/main/java/com/dmc/model/MetaData.java
@@ -0,0 +1,78 @@
+package com.dmc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+/**
+ * This is a simple class , which main task is to bound a key and a value
+ * together in one object , So it can be mapped throw JAXB (ASTRA XML).
+ *
+ * @author Mohamed Hussein
+ * @version 1.0
+ * @since 7/9/2018
+ */
+@Entity
+@Table(
+		indexes = {@Index(name= "metadata_Index",columnList= "md_key,md_value",unique= false)}
+		)
+public class MetaData {
+
+	private String id;
+	@Column(name="md_key")
+	private String key;
+	@Column(name="md_value")
+	private String value;
+
+	/**
+     * Get the ID of the object
+     * @return the id
+     */
+	public String getId() {
+		return id;
+	}
+
+	/**
+     * Set the ID of the object normally , it won't be set by user as ORM will generate the ID(as UUID of 40 char)
+     * @param id the id to set
+     */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+     * Get the Key
+     * @return the key
+     */
+	public String getKey() {
+		return key;
+	}
+
+	/**
+     * Set the Key 
+     * 
+     * the key will be something like :- epebDescription , ebepSeason , eAutoQC
+     * @param key the key to set
+     */
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	/**
+     * Get the value
+     * @return the value
+     */
+	public String getValue() {
+		return value;
+	}
+
+	 /**
+     * Set the value
+     * @param value the value to set
+     */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
